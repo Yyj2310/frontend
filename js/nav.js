08a113489d6cc4714f5d1613e223f0b2def683bf
Zane <17806539036@163.com>
2026-09-19 20:23:07 +0800

    进行了模块化的处理

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,14 @@
-(function () {
+// (function () {
+//   var path = location.pathname.split("/").pop() || "index.html";
+//   var links = document.querySelectorAll(".nav-link");
+//   for (var i = 0; i < links.length; i++) {
+//     var href = links[i].getAttribute("href");
+//     if (href === path) links[i].classList.add("active");
+//     else links[i].classList.remove("active");
+//   }
+// })();
+
+export function initNav() {
   var path = location.pathname.split("/").pop() || "index.html";
   var links = document.querySelectorAll(".nav-link");
   for (var i = 0; i < links.length; i++) {
@@ -6,4 +16,4 @@
     if (href === path) links[i].classList.add("active");
     else links[i].classList.remove("active");
   }
-})();
+}
